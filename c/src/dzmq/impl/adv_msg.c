c8b6ea94332f56ddab1d1df3713d0557e382e4c4
William Woodall <wjwwood@gmail.com>
2014-01-02 19:24:18 -0800

    c: use two bytes for address lengths

--- c/src/dzmq/impl/adv_msg.c
+++ c/src/dzmq/impl/adv_msg.c
@@ -6,9 +6,9 @@
 size_t serialize_adv_msg(uint8_t * buffer, dzmq_adv_msg_t * adv_msg)
 {
     size_t bytes_written = serialize_msg_header(buffer, &adv_msg->header);
-    uint8_t addr_len = strlen(adv_msg->addr);
-    memcpy(buffer + bytes_written, &addr_len, 1);
-    bytes_written += 1;
+    uint16_t addr_len = strlen(adv_msg->addr);
+    memcpy(buffer + bytes_written, &addr_len, sizeof(addr_len));
+    bytes_written += sizeof(addr_len);
     memcpy(buffer + bytes_written, adv_msg->addr, addr_len);
     bytes_written += addr_len;
     return bytes_written;
@@ -17,11 +17,11 @@ size_t serialize_adv_msg(uint8_t * buffer, dzmq_adv_msg_t * adv_msg)
 size_t deserialize_adv_msg(dzmq_adv_msg_t * adv_msg, uint8_t * buffer, size_t len)
 {
     size_t msg_len = 0, available_bytes = len;
-    uint8_t addr_len;
-    assert (available_bytes > 1);
-    memcpy(&addr_len, buffer, 1);
-    msg_len += 1;
-    available_bytes -= 1;
+    uint16_t addr_len;
+    assert (available_bytes > sizeof(addr_len));
+    memcpy(&addr_len, buffer, sizeof(addr_len));
+    msg_len += sizeof(addr_len);
+    available_bytes -= sizeof(addr_len);
     assert (available_bytes >= addr_len);
     memcpy(&adv_msg->addr, buffer + msg_len, addr_len);
     adv_msg->addr[addr_len] = 0;
