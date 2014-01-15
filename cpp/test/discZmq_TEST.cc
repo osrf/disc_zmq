99fcead84fe156e7cd4936463ec6db94a4500c8c
Carlos Agüero <caguero@osrfoundation.org>
2014-01-15 10:35:29 -0800

    Unit tests for discZmq.

--- cpp/test/discZmq_TEST.cc
+++ cpp/test/discZmq_TEST.cc
@@ -2,12 +2,111 @@
 #include "../discZmq.hh"
 #include "gtest/gtest.h"
 
-TEST(PacketTest, Basic)
+bool callbackExecuted;
+
+//  ---------------------------------------------------------------------
+/// \brief Function is called everytime a topic update is received.
+void cb(const std::string &_topic, const std::string &_data)
 {
-  EXPECT_EQ(1, 1);
+  assert(_topic != "");
+  EXPECT_EQ(_data, "someData");
+  callbackExecuted = true;
 }
 
-/////////////////////////////////////////////////
+//  ---------------------------------------------------------------------
+TEST(DiscZmqTest, PubWithoutAdvertise)
+{
+	std::string master = "";
+	bool verbose = false;
+	std::string topic1 = "foo";
+	std::string data = "someData";
+
+	// Subscribe to topic1
+	Node node(master, verbose);
+
+	// Publish some data on topic1 without advertising it first
+	EXPECT_NE(node.Publish(topic1, data), 0);
+}
+
+//  ---------------------------------------------------------------------
+TEST(DiscZmqTest, PubSub)
+{
+	callbackExecuted = false;
+	std::string master = "";
+	bool verbose = false;
+	std::string topic1 = "foo";
+	std::string data = "someData";
+
+	// Subscribe to topic1
+	Node node(master, verbose);
+	EXPECT_EQ(node.Subscribe(topic1, cb), 0);
+	node.SpinOnce();
+
+	// Advertise and publish some data on topic1
+	EXPECT_EQ(node.Advertise(topic1), 0);
+	EXPECT_EQ(node.Publish(topic1, data), 0);
+	s_sleep(100);
+	node.SpinOnce();
+
+	// Check that the data was received
+	EXPECT_TRUE(callbackExecuted);
+	callbackExecuted = false;
+
+	// Publish a second message on topic1
+	EXPECT_EQ(node.Publish(topic1, data), 0);
+	s_sleep(100);
+	node.SpinOnce();
+
+	// Check that the data was received
+	EXPECT_TRUE(callbackExecuted);
+	callbackExecuted = false;
+
+	// Unadvertise topic1 and publish a third message
+	node.UnAdvertise(topic1);
+	EXPECT_NE(node.Publish(topic1, data), 0);
+	s_sleep(100);
+	node.SpinOnce();
+	EXPECT_FALSE(callbackExecuted);
+}
+
+//  ---------------------------------------------------------------------
+/*TEST(DiscZmqTest, NPubSub)
+{
+	callbackExecuted = false;
+	std::string master = "";
+	bool verbose = false;
+	std::string topic1 = "foo";
+	std::string data = "someData";
+
+	// Subscribe to topic1
+	Node nodeSub(master, verbose);
+	EXPECT_EQ(nodeSub.Subscribe(topic1, cb), 0);
+	nodeSub.SpinOnce();
+
+	// Advertise and publish some data on topic1
+	Node *nodePub = new Node(master, verbose);
+	EXPECT_EQ(nodePub->Advertise(topic1), 0);
+	EXPECT_EQ(nodePub->Publish(topic1, data), 0);
+	s_sleep(200);
+	nodeSub.SpinOnce();
+
+	// Check that the data was received
+	EXPECT_TRUE(callbackExecuted);
+	callbackExecuted = false;
+	delete nodePub;
+
+	// Publish a second message on topic1 with a new node
+	nodePub = new Node(master, verbose);
+	EXPECT_EQ(nodePub->Advertise(topic1), 0);
+	EXPECT_EQ(nodePub->Publish(topic1, data), 0);
+	s_sleep(100);
+	nodeSub.SpinOnce();
+
+	// Check that the data was received
+	EXPECT_TRUE(callbackExecuted);
+}*/
+
+//  ---------------------------------------------------------------------
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
--- cpp/test/topicsInfo_TEST.cc
+++ cpp/test/topicsInfo_TEST.cc
@@ -2,18 +2,22 @@
 #include "../topicsInfo.hh"
 #include "gtest/gtest.h"
 
+//  ---------------------------------------------------------------------
 void myCb(const std::string &p1, const std::string &p2)
 {
 }
 
+//  ---------------------------------------------------------------------
 void myReqCb(const std::string &p1, int p2, const std::string &p3)
 {
 }
 
+//  ---------------------------------------------------------------------
 int myRepCb(const std::string &p1, const std::string &p2, std::string &p3)
 {
 }
 
+//  ---------------------------------------------------------------------
 TEST(PacketTest, BasicTopicsInfoAPI)
 {
   TopicsInfo topics;
@@ -25,96 +29,96 @@ TEST(PacketTest, BasicTopicsInfoAPI)
   TopicInfo::RepCallback repCb;
 
   // Check getters with an empty TopicsInfo object
-  EXPECT_EQ(topics.HasTopic(topic), false);
-  EXPECT_EQ(topics.GetAdvAddresses(topic, v), false);
-  EXPECT_EQ(topics.HasAdvAddress(topic, address), false);
-  EXPECT_EQ(topics.Connected(topic), false);
-  EXPECT_EQ(topics.Subscribed(topic), false);
-  EXPECT_EQ(topics.AdvertisedByMe(topic), false);
-  EXPECT_EQ(topics.Requested(topic), false);
-  EXPECT_EQ(topics.GetCallback(topic, cb), false);
-  EXPECT_EQ(topics.GetReqCallback(topic, reqCb), false);
-  EXPECT_EQ(topics.GetRepCallback(topic, repCb), false);
-  EXPECT_EQ(topics.PendingReqs(topic), false);
+  EXPECT_FALSE(topics.HasTopic(topic));
+  EXPECT_FALSE(topics.GetAdvAddresses(topic, v));
+  EXPECT_FALSE(topics.HasAdvAddress(topic, address));
+  EXPECT_FALSE(topics.Connected(topic));
+  EXPECT_FALSE(topics.Subscribed(topic));
+  EXPECT_FALSE(topics.AdvertisedByMe(topic));
+  EXPECT_FALSE(topics.Requested(topic));
+  EXPECT_FALSE(topics.GetCallback(topic, cb));
+  EXPECT_FALSE(topics.GetReqCallback(topic, reqCb));
+  EXPECT_FALSE(topics.GetRepCallback(topic, repCb));
+  EXPECT_FALSE(topics.PendingReqs(topic));
 
   // Check getters after inserting a topic in a TopicsInfo object
   topics.AddAdvAddress(topic, address);
-  EXPECT_EQ(topics.HasTopic(topic), true);
-  EXPECT_EQ(topics.HasAdvAddress(topic, address), true);
-  EXPECT_EQ(topics.GetAdvAddresses(topic, v), true);
+  EXPECT_TRUE(topics.HasTopic(topic));
+  EXPECT_TRUE(topics.HasAdvAddress(topic, address));
+  EXPECT_TRUE(topics.GetAdvAddresses(topic, v));
   EXPECT_EQ(v.at(0), address);
-  EXPECT_EQ(topics.Connected(topic), false);
-  EXPECT_EQ(topics.Subscribed(topic), false);
-  EXPECT_EQ(topics.AdvertisedByMe(topic), false);
-  EXPECT_EQ(topics.Requested(topic), false);
-  EXPECT_EQ(topics.GetCallback(topic, cb), false);
-  EXPECT_EQ(topics.GetReqCallback(topic, reqCb), false);
-  EXPECT_EQ(topics.GetRepCallback(topic, repCb), false);
-  EXPECT_EQ(topics.PendingReqs(topic), false);
+  EXPECT_FALSE(topics.Connected(topic));
+  EXPECT_FALSE(topics.Subscribed(topic));
+  EXPECT_FALSE(topics.AdvertisedByMe(topic));
+  EXPECT_FALSE(topics.Requested(topic));
+  EXPECT_FALSE(topics.GetCallback(topic, cb));
+  EXPECT_FALSE(topics.GetReqCallback(topic, reqCb));
+  EXPECT_FALSE(topics.GetRepCallback(topic, repCb));
+  EXPECT_FALSE(topics.PendingReqs(topic));
 
   // Check that there's only one copy stored of the same address
   topics.AddAdvAddress(topic, address);
-  EXPECT_EQ(topics.GetAdvAddresses(topic, v), true);
+  EXPECT_TRUE(topics.GetAdvAddresses(topic, v));
   EXPECT_EQ(v.size(), 1);
 
   // Check SetConnected
   topics.SetConnected(topic, true);
-  EXPECT_EQ(topics.Connected(topic), true);
+  EXPECT_TRUE(topics.Connected(topic));
 
   // Check SetSubscribed
   topics.SetSubscribed(topic, true);
-  EXPECT_EQ(topics.Subscribed(topic), true);
+  EXPECT_TRUE(topics.Subscribed(topic));
 
   // Check SetRequested
   topics.SetRequested(topic, true);
-  EXPECT_EQ(topics.Requested(topic), true);
+  EXPECT_TRUE(topics.Requested(topic));
 
   // Check SetAdvertisedByMe
   topics.SetAdvertisedByMe(topic, true);
-  EXPECT_EQ(topics.AdvertisedByMe(topic), true);
+  EXPECT_TRUE(topics.AdvertisedByMe(topic));
 
   // Check SetCallback
   topics.SetCallback(topic, myCb);
-  EXPECT_EQ(topics.GetCallback(topic, cb), true);
+  EXPECT_TRUE(topics.GetCallback(topic, cb));
   EXPECT_EQ(&myCb, cb);
 
 	// Check SetReqCallback
   topics.SetReqCallback(topic, myReqCb);
-  EXPECT_EQ(topics.GetReqCallback(topic, reqCb), true);
+  EXPECT_TRUE(topics.GetReqCallback(topic, reqCb));
   EXPECT_EQ(&myReqCb, reqCb);
 
 	// Check SetRepCallback
   topics.SetRepCallback(topic, myRepCb);
-  EXPECT_EQ(topics.GetRepCallback(topic, repCb), true);
+  EXPECT_TRUE(topics.GetRepCallback(topic, repCb));
   EXPECT_EQ(&myRepCb, repCb);
 
   // Check the address removal
   topics.RemoveAdvAddress(topic, address);
-  EXPECT_EQ(topics.HasAdvAddress(topic, address), false);
-  EXPECT_EQ(topics.GetAdvAddresses(topic, v), false);
+  EXPECT_FALSE(topics.HasAdvAddress(topic, address));
+  EXPECT_FALSE(topics.GetAdvAddresses(topic, v));
 
   // Check the addition of asynchronous service call requests
   std::string param1 = "param1";
   std::string param2 = "param2";
-  EXPECT_EQ(topics.DelReq(topic, param1), false);
+  EXPECT_FALSE(topics.DelReq(topic, param1));
   for (TopicInfo::Topics_M_it it = topics.GetTopics().begin();
        it != topics.GetTopics().end(); ++it)
-  	EXPECT_EQ(topics.PendingReqs(it->first), false);
+  	EXPECT_FALSE(topics.PendingReqs(it->first));
   topics.AddReq(topic, param1);
   for (TopicInfo::Topics_M_it it = topics.GetTopics().begin();
        it != topics.GetTopics().end(); ++it)
-  	EXPECT_EQ(topics.PendingReqs(it->first), true);
+  	EXPECT_TRUE(topics.PendingReqs(it->first));
   topics.AddReq(topic, param2);
   for (TopicInfo::Topics_M_it it = topics.GetTopics().begin();
        it != topics.GetTopics().end(); ++it)
-  	EXPECT_EQ(topics.PendingReqs(it->first), true);
+  	EXPECT_TRUE(topics.PendingReqs(it->first));
 
-  EXPECT_EQ(topics.DelReq(topic, param2), true);
-  EXPECT_EQ(topics.DelReq(topic, param1), true);
-  EXPECT_EQ(topics.DelReq(topic, param1), false);
+  EXPECT_TRUE(topics.DelReq(topic, param2));
+  EXPECT_TRUE(topics.DelReq(topic, param1));
+  EXPECT_FALSE(topics.DelReq(topic, param1));
 }
 
-/////////////////////////////////////////////////
+//  ---------------------------------------------------------------------
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
