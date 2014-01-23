29d72b686c095b7a06d6454e6c6c92669361c849
Carlos Agüero <caguero@osrfoundation.org>
2014-01-23 14:49:43 -0800

    Some tweaking.

--- cpp/test/test-dns-sd.py
+++ cpp/test/test-dns-sd.py
@@ -6,25 +6,24 @@ Testing multiple instances of mDNS clients.
 import argparse
 import subprocess
 
-def go(_prefix_name, _type, _domain, _port, _n, _verbose):
+def go(_prefix_name, _service, _domain, _port, _n):
     '''
     '''
-    if _verbose:
-        print ('Registering %d services:' % _n)
-        print ('\t Prefix_name: %s' % _prefix_name)
-        print ('\t Type: %s' % _type)
-        print ('\t Port: %s' % _port)
+    print ('Registering %s service/s:' % _n)
+    print ('\t Prefix_name: %s' % _prefix_name)
+    print ('\t Type: %s' % _service)
+    print ('\t Port: %d' % _port)
 
-    for i in range(_n):
+    for i in range(int(_n)):
         try:
             # Updload the text file into the VRC Portal
-            cmd = ('dns-sd -R ' + _prefix_name + str(i) + ' ' + _type + ' ' +
-                   _domain + ' ' + _port)
+            cmd = ('dns-sd -R ' + _prefix_name + str(i) + ' ' + _service + ' ' +
+                   _domain + ' ' + str(_port))
 
             subprocess.check_call(cmd.split())
 
         except Exception, excep:
-            print ('%sError in dns-sd operation : %s'
+            print ('Error in dns-sd operation : %s'
                    % repr(excep))
         _port = _port + 1
 
@@ -36,23 +35,19 @@ if __name__ == '__main__':
         description=('Run multiple instances of mDNS clients'))
 
     parser.add_argument('prefix_name', help='Prefix name of the instance')
-    parser.add_argument('type', help='Service type')
+    parser.add_argument('service', help='Service type')
     parser.add_argument('domain', help='Service domain')
     parser.add_argument('port', help='Starting port')
     parser.add_argument('n', help='Number of instances')
 
-    parser.add_argument('-v', '--verbose',
-                        help='Enable verbose mode', default=False)
-
     # Parse command line arguments
     args = parser.parse_args()
     arg_prefix_name = args.prefix_name
-    arg_type = args.type
+    arg_service = args.service
     arg_domain = args.domain
     arg_port = args.port
     arg_n = args.n
-    arg_verbose = args.verbose
 
 
     # Attach the cloudsims!
-    go(arg_prefix_name, arg_type, arg_domain, arg_port, arg_n, arg_verbose)
+    go(arg_prefix_name, arg_service, arg_domain, int(arg_port), arg_n)
